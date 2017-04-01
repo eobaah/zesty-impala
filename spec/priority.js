748bac17538f1257cc142afa87b309e4dad5226e
eobaah <eobaah@gmail.com>
2017-03-31 18:13:30 -0700

    just completed tests for encueue

--- spec/priority.js
+++ spec/priority.js
@@ -12,71 +12,153 @@ describe('PriorityQueue', () => {
   })
 
   context('enqueue()', () => {
-    it.only('Adds an element with priority number to the back of the queue.', () => {
+    it('Adds an element with priority number to the back of an empty queue.', () => {
       const myPQueue = new PriorityQueue()
       myPQueue.enqueue("foo", 100)
-      expect(myPQueue.head.priority).to.equal(100)
+      expect(myPQueue.head.priority).to.equal(100) && expect(myPQueue.head.priority).to.equal(100)
+    })
+  })
+
+  context('enqueue()', () => {
+    it('Adds an element with priority number to the back of the queue.', () => {
+      const myPQueue = new PriorityQueue()
+      myPQueue.enqueue("foo", 100)
+      myPQueue.enqueue("foo", 75)
+      expect(myPQueue.head.priority).to.equal(100) &&
+      expect(myPQueue.tail.priority).to.equal(75) &&
+      expect(myPQueue.size).to.equal(2)
+
+    })
+  })
+
+  context('enqueue()', () => {
+    it('Adds an element with priority number to the back of a queue with more than one node in the queue.', () => {
+      const myPQueue = new PriorityQueue()
+      myPQueue.enqueue("foo", 100)
+      myPQueue.enqueue("roo", 75)
+      myPQueue.enqueue("boo", 60)
+      myPQueue.enqueue("goo", 50)
+      expect(myPQueue.head.priority).to.equal(100) &&
+      expect(myPQueue.tail.priority).to.equal(50) &&
+      expect(myPQueue.tail.next.next.priority).to.equal(75) &&
+      expect(myPQueue.size).to.equal(4)
+
+    })
+  })
+
+  context('enqueue()', () => {
+    it('Adds an element with priority number to the front of a queue.', () => {
+      const myPQueue = new PriorityQueue()
+      myPQueue.enqueue("foo", 100)
+      myPQueue.enqueue("roo", 75)
+      myPQueue.enqueue("bam", 125)
+      expect(myPQueue.head.priority).to.equal(125) &&
+      expect(myPQueue.tail.priority).to.equal(75) &&
+      expect(myPQueue.size).to.equal(3)
+    })
+  })
+
+  context('front()', () => {
+    it.only('Returns the front element in queue with the highest priority', () => {
+      const front = new PriorityQueue()
+      front.enqueue("foo", 500)
+      expect(front.front().priority).to.equal(500) &&
+      expect(front.size).to.equal(1)
+    })
+  })
+
+  context('front()', () => {
+    it('Returns the front element in a queue with two nodes and the highest priority', () => {
+      const front = new PriorityQueue()
+      front.enqueue("foo", 500)
+      front.enqueue("boo", 400)
+      expect(front.front().priority).to.equal(500) &&
+      expect(front.size).to.equal(2)
+    })
+  })
+
+  context('front()', () => {
+    it('Returns the front element in queue with the highest priority and ensures the tail is the same', () => {
+      const front = new PriorityQueue()
+      front.enqueue("foo", 500)
+      front.enqueue("boo", 400)
+      expect(front.front().tail.data).to.equal("boo") &&
+      expect(front.size).to.equal(2)
+    })
+  })
+
+  context('back()', () => {
+    it('Returns the last element in queue with the lowest priority', () => {
+      const back = new PriorityQueue()
+      back.enqueue("foo", 500)
+      back.enqueue("boo", 400)
+      expect(back.back().priority).to.equal(400) &&
+      expect(back.size).to.equal(2)
+    })
+  })
+
+  context('back()', () => {
+    it('Check the last element in a queue with multiple items', () => {
+      const back = new PriorityQueue()
+      back.enqueue("foo", 500)
+      back.enqueue("boo", 400)
+      back.enqueue("BMW", 300)
+      expect(back.back().tail.data).to.equal("BMW") &&
+      expect(back.head.data).to.equal("foo") &&
+      expect(back.size).to.equal(3)
     })
   })
 
   context('dequeue()', () => {
-    it('Returns and removes the front element with the highest priority in the queue or null if the queue is empty', () => {
-      const dequeue = new PriorityQueue()
-      dequeue.enqueue("foo")
-      expect(dequeue.dequeue().data).to.equal("foo")
+    it('Returns and removed an element with priority number from the front of the queue.', () => {
+      const myDequeue = new PriorityQueue()
+      myDequeue.enqueue("foo", 100)
+      expect(myDequeue.dequeue().data).to.equal("foo") &&
+      expect(myDequeue.size).to.equal(0)
     })
   })
 
-  // context('front()', () => {
-  //   it('Returns the front element in queue', () => {
-  //     const front = new PriorityQueue()
-  //     front.enqueue("foo")
-  //     expect(front.front().data).to.equal("foo")
-  //   })
-  // })
-  //
-  // context('front()', () => {
-  //   it('Check the front and returns null if the queue is empty', () => {
-  //     const front = new PriorityQueue()
-  //     expect(front.front()).to.equal(null)
-  //   })
-  // })
-  //
-  // context('back()', () => {
-  //   it('Returns the back element in the queue.', () => {
-  //     const back = new PriorityQueue()
-  //     back.enqueue("foo")
-  //     expect(back.back().data).to.equal("foo")
-  //   })
-  // })
-  //
-  // context('back()', () => {
-  //   it('Returns null for the back element of an empty queue.', () => {
-  //     const back = new PriorityQueue()
-  //     expect(back.back()).to.equal(null)
-  //   })
-  // })
-  //
-  // context('isEmpty()', () => {
-  //   it('Returns false if the queue is not empty. ', () => {
-  //     const isEmpty = new PriorityQueue()
-  //     isEmpty.enqueue('foo')
-  //     expect(isEmpty.isEmpty()).to.equal(false);
-  //   })
-  // })
-  //
-  // context('isEmpty()', () => {
-  //   it('Returns true if the queue is empty. ', () => {
-  //     const isEmpty = new PriorityQueue()
-  //     expect(isEmpty.isEmpty()).to.equal(true);
-  //   })
-  // })
-  //
-  // context('length()', () => {
-  //   it('returns the number of elements in the queue ', () => {
-  //     const length = new PriorityQueue()
-  //     length.enqueue("foo")
-  //     length.enqueue("roo")
-  //     expect(length.length()).to.equal(2);
-  //   })
+  context('dequeue()', () => {
+    it('Returns and removed an element with priority number from the front of the queue.', () => {
+      const myDequeue = new PriorityQueue()
+      myDequeue.enqueue("foo", 100)
+      myDequeue.enqueue("boo", 200)
+      myDequeue.enqueue("BMW", 300)
+      expect(myDequeue.dequeue().data).to.equal("BMW") &&
+      expect(myDequeue.dequeue.head).to.equal("boo") &&
+      expect(myDequeue.size).to.equal(2)
+    })
+  })
+
+  context('isEmpty()', () => {
+    it('Returns false for a queue that is not empty.', () => {
+      const myDequeue = new PriorityQueue()
+      myDequeue.enqueue("foo", 100)
+      expect(myDequeue.isEmpty()).to.equal(false) &&
+      expect(myDequeue.size).to.equal(1)
+    })
+  })
+
+  context('isEmpty()', () => {
+    it('Returns true for queue that is empty .', () => {
+      const myDequeue = new PriorityQueue()
+      expect(myDequeue.isEmpty()).to.equal(true) &&
+      expect(myDequeue.size).to.equal(0)
+    })
+  })
+
+  context('length()', () => {
+    it('returns the number of elements in the queue ', () => {
+      const length = new PriorityQueue()
+      length.enqueue("foo")
+      length.enqueue("roo")
+      expect(length.length()).to.equal(2);
+  })
+})
+  context('length()', () => {
+    it('returns the zero for an empty queue ', () => {
+      const length = new PriorityQueue()
+      expect(length.length()).to.equal(0);
+  })
+})
 })
--- src/priority.js
+++ src/priority.js
@@ -9,68 +9,57 @@ class PriorityNode {
 }
 
 export default class PriorityQueue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0
+  }
 
-    constructor(data) {
-      this.head = null
-      this.tail = null
-      this.size = 0
+  enqueue(data, priority) {
+    let newNode = new PriorityNode(data, priority)
+    this.size++
+    let cursor = this.tail;
+
+    // if queue is empty
+    if (!this.head) {
+      this.head = newNode
+      this.tail = newNode
+      return
     }
 
+    // can we add node to the end?
+    if (this.tail.priority > newNode.priority) {
+      newNode.next = this.tail
+      this.tail = newNode
+      return
+    }
+    // can we add to the front?
+    if (this.head.priority < newNode.priority){
+      this.head.next = newNode;
+      this.head = newNode;
+      return
+    }
+    // can we add in the middle?
 
-    enqueue( data, priority ) {
-      let newNode = new PriorityNode(data, priority)
-
-      if (this.size === 0) {
-        // QUEUE IS EMPTY
-        this.tail = newNode
-        this.head = newNode
-        this.size++
-        return
-      }
-
-      if (this.tail.priority > newNode.priority) {
-        // NEW NODE IS THE NEW TAIL
-        newNode.next = this.tail
-        this.tail = newNode
-        this.size++
-        return
-      }
-
-      if (newNode.priority > this.head.priority) {
-        // NEW NODE IS THE NEW HEAD
-        this.head.next = newNode
-        this.head = newNode
-        this.size++
-        return
-      }
-
-      // SPLICE SOMEWHERE IN THE MIDDLE...
-      let cursor = this.tail
-      console.log('newNode', newNode)
-      while(cursor) {
-        if (
-          newNode.priority > cursor.priority &&
-          (cursor.next && newNode.priority > cursor.next.priority)
-        ){
-          newNode.next = cursor.next
-          cursor.next = newNode
-          this.size++
-          return
-        }
+    while (cursor){
+      if (cursor.priority < newNode.priority && cursor.next.priority < newNode.priority) {
         cursor = cursor.next
+      } else {
+        newNode.next = cursor.next
+        cursor.next = newNode
+        return
       }
+      }
+  }
 
-    }
+  
 }
 
 
+const myQueue = new PriorityQueue()
 
-  const pQueue = new PriorityQueue()
-
-  pQueue.enqueue("pizza", 200)
-
-  pQueue.enqueue("Beer", 198)
-
-  pQueue.enqueue("cheese", 300)
-
-  pQueue.enqueue("kale", 250)
+myQueue.enqueue("Tesla", 170)
+myQueue.enqueue("foo", 100)
+myQueue.enqueue("boo", 75)
+myQueue.enqueue("bmw", 70)
+myQueue.enqueue("Ford", 165)
