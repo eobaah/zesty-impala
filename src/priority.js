e1c33bc4747f038009283e91e59b75fb19bfae1a
eobaah <eobaah@gmail.com>
2017-04-03 11:56:11 -0700

    add gitignore removing node modules

--- src/priority.js
+++ src/priority.js
@@ -3,119 +3,79 @@
 class PriorityNode {
   constructor(data, priority) {
     this.data = data;
-    this.priority = priority;
     this.next = null;
+    this.priority = priority;
   }
 }
 
-export default class PriorityQueue {
-  constructor() {
-    this.head = null;
-    this.tail = null;
+export default class Priority {
+
+  constructor(data) {
+    this.head = null
+    this.tail = null
     this.size = 0
   }
 
-  enqueue(data, priority) {
-    let newNode = new PriorityNode(data, priority)
-    this.size++
-    let cursor = this.tail;
-
-    // if queue is empty
-    if (!this.head) {
-      this.head = newNode
-      this.tail = newNode
-      return
+  enqueue( data, priority ) {
+    const newPriorityNode = new PriorityNode( data, priority ) //{ data: 'foo', next: null}
+    if( this.size === 0 ) {
+      this.head = newPriorityNode
+      this.tail = newPriorityNode
+      this.size++
+    } else {
+      newPriorityNode.next = this.tail // newPriorityNode = { data: 'foo', next:{data: 'roo', next: null}}
+      //this.tail = {{data: 'roo', next: null}
+      this.tail = newPriorityNode
+      this.size++
     }
+  }
 
-    // can we add node to the end?
-    if (this.tail.priority > newNode.priority) {
-      newNode.next = this.tail
-      this.tail = newNode
-      return
+  dequeue() {
+    if( this.size === 0 ) {
+      return null
+    } else {
+      let removedNode = this.head
+      this.head = this.next;
+      this.size--;
+      return removedNode
     }
-    // can we add to the front?
-    if (this.head.priority < newNode.priority){
-      this.head.next = newNode;
-      this.head = newNode;
-      return
-    }
-    // can we add in the middle?
-
-    while (cursor){
-      if (cursor.priority < newNode.priority && cursor.next.priority < newNode.priority) {
-        cursor = cursor.next
-      } else {
-        newNode.next = cursor.next
-        cursor.next = newNode
-        return
-      }
-      }
   }
 
   front() {
-    if(this.size === 0) {
-      return null
+    if(this.head) {
+      return this.head;
     } else {
-      return this.head
+      return null;
     }
   }
 
-  back() {
-    if(this.size === 0) {
-      return null
+  back(){
+    if(this.tail) {
+      return this.tail;
     } else {
-      return this.tail
+      return null;
     }
-  }
-  dequeue() {
-    let removedHead = this.head
-    let cursor = this.tail;
 
-  if (this.size === 0) {
-    return null
   }
 
-  if (this.size === 1 ) {
-    this.head = null
-    this.tail = null
-    this.size --
-    return removedHead
-  }
-
-  if (this.size === 2 ) {
-    this.head = this.tail
-    this.tail.next = null
-    this.size --
-    return removedHead
-  }
-  while(cursor.next.next) {
-    cursor = cursor.next
-  }
-
-  cursor.next = null
-  this.head = cursor
-  this.size--
-  return removedHead
-  }
-
-  isEmpty() {
-    if(this.size === 0) {
+  isEmpty(){
+    if () {
       return true
     } else {
       return false
     }
   }
 
-  length() {
-    return this.size 
+  length(){
+    return this.size
   }
-
 }
 
-const myQueue = new PriorityQueue()
 
-myQueue.enqueue("Tesla", 170)
-myQueue.enqueue("foo", 100)
-myQueue.enqueue("boo", 75)
-myQueue.enqueue("bmw", 70)
-myQueue.enqueue("Ford", 165)
+
+
+const queue = new Queue()
+
+queue.enqueue('foo')
+// bayarea.push('fred')
+// bayarea.push('BMW')
